acc66009086cd487341dd2dd6dd2d537365a4941
ashishsonani <ashish.patel0903@gmail.com>
2019-01-28 15:52:03 +0530

    Product name app bar added and some ui changes

--- app/src/main/java/com/example/agrify/activity/StoreDetailActivity.java
+++ app/src/main/java/com/example/agrify/activity/StoreDetailActivity.java
@@ -1,8 +1,10 @@
 package com.example.agrify.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -22,9 +24,8 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.ListenerRegistration;
 import com.google.firebase.firestore.Query;
 
-public class StoreDetailActivity extends AppCompatActivity  implements EventListener<DocumentSnapshot> {
+public class StoreDetailActivity extends AppCompatActivity implements EventListener<DocumentSnapshot> {
     private static final String TAG = "StoreDetail";
-
     public static final String KEY_STORE_ID = "key_store_id";
 
 
@@ -34,10 +35,11 @@ public class StoreDetailActivity extends AppCompatActivity  implements EventList
     ActivityStoreDetailBinding bind;
     Query sellerQuery;
     SellerAdapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-      bind = DataBindingUtil.setContentView(this, R.layout.activity_store_detail);
+        bind = DataBindingUtil.setContentView(this, R.layout.activity_store_detail);
         bind.productDes.setMovementMethod(new ScrollingMovementMethod());
 
 
@@ -50,17 +52,24 @@ public class StoreDetailActivity extends AppCompatActivity  implements EventList
 
         // Get reference to the restaurant
         mStoreRef = mFirestore.collection("store").document(storeId);
-      getSellerList();
+        getSellerList();
 
+        bind.appBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
 
     @Override
     public void onStart() {
         super.onStart();
-          mAdapter.startListening();
+        mAdapter.startListening();
         mStoreRegistration = mStoreRef.addSnapshotListener(this);
     }
+
     @Override
     public void onEvent(DocumentSnapshot snapshot, FirebaseFirestoreException e) {
         if (e != null) {
@@ -81,16 +90,13 @@ public class StoreDetailActivity extends AppCompatActivity  implements EventList
     }
 
 
-    void getSellerList()
-    {
+    void getSellerList() {
         sellerQuery = mStoreRef
                 .collection("sellerList")
                 .orderBy("name", Query.Direction.DESCENDING);
-          mAdapter =new SellerAdapter(sellerQuery,this);
+        mAdapter = new SellerAdapter(sellerQuery, this);
         bind.sellerListRecycleView.setLayoutManager(new LinearLayoutManager(this));
         bind.sellerListRecycleView.setAdapter(mAdapter);
-
-
 
 
     }
