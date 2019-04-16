e06f70ab5bdaa8b7bda0fd29e284eca88ea3fac5
arjun shah <arjun.18shah@gmail.com>
2019-04-16 22:38:21 +0530

    added

--- app/src/main/java/com/example/agrify/activity/model/Seller.java
+++ app/src/main/java/com/example/agrify/activity/model/Seller.java
@@ -4,85 +4,13 @@ import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.DocumentReference;
 
 public class Seller {
-    private String name, profilePhotoUrl, phone;
-    private DocumentReference userId,id;
-    private Timestamp timestamp;
+    private String name, profilePhotoUrl, phone,email,info,productId;
+    private DocumentReference StoreProductRef,SellerProductRef,AddressRef;
+
     private float price;
     private int stock,minQuantity,maxQuantity,imageCount;
 
-
-
     public Seller() {
-    }
-
-    public Seller(String name, String profilePhotoUrl, DocumentReference userId, Timestamp timestamp, float price, String phone,int stock,int minQuantity,int maxQuantity,int imageCount,DocumentReference id) {
-        this.name = name;
-        this.profilePhotoUrl = profilePhotoUrl;
-        this.userId = userId;
-        this.timestamp = timestamp;
-        this.price = price;
-        this.phone = phone;
-        this.stock=stock;
-        this.minQuantity=minQuantity;
-        this.maxQuantity=maxQuantity;
-        this.imageCount=imageCount;
-        this.id=id;
-    }
-
-    public DocumentReference getId() {
-        return id;
-    }
-
-    public void setId(DocumentReference id) {
-        this.id = id;
-    }
-
-    public int getMaxQuantity() {
-        return maxQuantity;
-    }
-
-    public int getImageCount() {
-        return imageCount;
-    }
-
-    public void setImageCount(int imageCount) {
-        this.imageCount = imageCount;
-    }
-
-    public void setMaxQuantity(int maxQuantity) {
-        this.maxQuantity = maxQuantity;
-    }
-
-    public int getMinQuantity() {
-        return minQuantity;
-    }
-
-    public void setMinQuantity(int minQuantity) {
-        this.minQuantity = minQuantity;
-    }
-
-    public int getStock() {
-        return stock;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getProfilePhotoUrl() {
@@ -93,20 +21,68 @@ public class Seller {
         this.profilePhotoUrl = profilePhotoUrl;
     }
 
-    public DocumentReference getUserId() {
-        return userId;
+    public String getName() {
+        return name;
     }
 
-    public void setUserId(DocumentReference userId) {
-        this.userId = userId;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public Timestamp getTimestamp() {
-        return timestamp;
+    public String getPhone() {
+        return phone;
     }
 
-    public void setTimestamp(Timestamp timestamp) {
-        this.timestamp = timestamp;
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public DocumentReference getStoreProductRef() {
+        return StoreProductRef;
+    }
+
+    public void setStoreProductRef(DocumentReference storeProductRef) {
+        StoreProductRef = storeProductRef;
+    }
+
+    public DocumentReference getSellerProductRef() {
+        return SellerProductRef;
+    }
+
+    public void setSellerProductRef(DocumentReference sellerProductRef) {
+        SellerProductRef = sellerProductRef;
+    }
+
+    public DocumentReference getAddressRef() {
+        return AddressRef;
+    }
+
+    public void setAddressRef(DocumentReference addressRef) {
+        AddressRef = addressRef;
     }
 
     public float getPrice() {
@@ -115,5 +91,54 @@ public class Seller {
 
     public void setPrice(float price) {
         this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getMinQuantity() {
+        return minQuantity;
+    }
+
+    public void setMinQuantity(int minQuantity) {
+        this.minQuantity = minQuantity;
+    }
+
+    public int getMaxQuantity() {
+        return maxQuantity;
+    }
+
+    public void setMaxQuantity(int maxQuantity) {
+        this.maxQuantity = maxQuantity;
+    }
+
+    public int getImageCount() {
+        return imageCount;
+    }
+
+    public void setImageCount(int imageCount) {
+        this.imageCount = imageCount;
+    }
+
+    public Seller(String name, String profilePhotoUrl, String phone, String email, String info, String productId, DocumentReference storeProductRef, DocumentReference sellerProductRef, DocumentReference addressRef, float price, int stock, int minQuantity, int maxQuantity, int imageCount) {
+        this.name = name;
+        this.profilePhotoUrl = profilePhotoUrl;
+        this.phone = phone;
+        this.email = email;
+        this.info = info;
+        this.productId = productId;
+        StoreProductRef = storeProductRef;
+        SellerProductRef = sellerProductRef;
+        AddressRef = addressRef;
+        this.price = price;
+        this.stock = stock;
+        this.minQuantity = minQuantity;
+        this.maxQuantity = maxQuantity;
+        this.imageCount = imageCount;
     }
 }
--- app/src/main/java/com/example/agrify/activity/sellerProduct/SellerProductActivity.java
+++ app/src/main/java/com/example/agrify/activity/sellerProduct/SellerProductActivity.java
@@ -2,31 +2,54 @@ package com.example.agrify.activity.sellerProduct;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.AbsListView;
 import android.widget.ImageView;
+import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import com.example.agrify.R;
+import com.example.agrify.activity.model.Seller;
+import com.example.agrify.activity.model.Store;
 import com.example.agrify.databinding.ActivitySellerProductBinding;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.smarteist.autoimageslider.DefaultSliderView;
 
+import javax.annotation.Nullable;
 
-public class SellerProductActivity extends AppCompatActivity {
+import es.dmoral.toasty.Toasty;
+
+
+public class SellerProductActivity extends AppCompatActivity  implements EventListener<DocumentSnapshot> {
     String seller_id;
     String product_id;
     FirebaseFirestore firebaseFirestore;
+    private DocumentReference mSellerProductRef;
     ActivitySellerProductBinding binding;
 
+    private ListenerRegistration sellerProductListener;
+    private ListenerRegistration productListener;
+    private DocumentReference productRef;
+    private GoogleMap map;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_seller_product);
         if (getIntent().getStringExtra("seller_id") != null && getIntent().getStringExtra("product_id") != null) {
@@ -35,10 +58,23 @@ public class SellerProductActivity extends AppCompatActivity {
             Log.i("product_id", product_id);
             Log.i("seller_id", seller_id);
         }
+
+
+setSupportActionBar(binding.appBar);
+
         firebaseFirestore = FirebaseFirestore.getInstance();
+        mSellerProductRef=firebaseFirestore.collection("store").document(product_id).collection("sellerList").document(seller_id);
+        productRef=firebaseFirestore.collection("store").document(product_id);
+
         //   binding.imageSlider.setIndicatorAnimation(SliderLayout.A); //set indicator animation by using SliderLayout.Animations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
         binding.imageSlider.setScrollTimeInSec(5);
         initSlider();
+        binding.appBar.setNavigationOnClickListener(v->{
+            onBackPressed();
+
+        });
+
+
 
     }
 
@@ -58,6 +94,7 @@ public class SellerProductActivity extends AppCompatActivity {
                                 binding.imageSlider.addSliderView(sliderView);
                             }
 
+
                         } else {
 
                         }
@@ -65,4 +102,48 @@ public class SellerProductActivity extends AppCompatActivity {
                     }
                 });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        productListener=productRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot snapshot, @Nullable FirebaseFirestoreException e) {
+                productLoader(snapshot);
+
+            }
+        });
+        sellerProductListener=mSellerProductRef.addSnapshotListener(this);
+
+    }
+String unit;
+    private void productLoader(DocumentSnapshot snapshot) {
+        Store store=snapshot.toObject(Store.class);
+        binding.productName.setText(store.getName());
+        unit=store.getUnit();
+    }
+
+    @Override
+    public void onEvent(@Nullable DocumentSnapshot snapshot, @Nullable FirebaseFirestoreException e) {
+        if(e!=null)
+        {
+            return;
+        }
+        Toasty.info(getApplicationContext(),"checkout again some data is changed",Toasty.LENGTH_SHORT).show();
+        sellerProductLoader(snapshot);
+
+
+    }
+
+    private void sellerProductLoader(DocumentSnapshot snapshot) {
+        Seller seller=snapshot.toObject(Seller.class);
+binding.minQuantity.setText("min quantity:"+String.valueOf(seller.getMinQuantity())+"/"+unit);
+binding.textInfo.setText(seller.getInfo());
+binding.appBar.setTitle(seller.getName());
+        binding.sellerPrice.setText(String.valueOf(seller.getPrice())+"/"+unit);
+
+
+    }
+
+
 }
